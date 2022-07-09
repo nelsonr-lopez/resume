dff8666606ae77a5edaabaa4052f835379aa6c3d
nelsonr.lopez <nelson@schweitzerlabs.com>
2022-07-09 19:19:11 -0400

    added skills refactor

--- src/lib/Skills.svelte
+++ src/lib/Skills.svelte
@@ -1,44 +1,18 @@
 <script>
   import data from "../content/data.json";
-  let { tools } = data;
+  let { tools, skills } = data;
 </script>
 
 <div class="font-light text-lg px-2 mb-12">
   <h2 class="text-xl font-semibold mb-4">Skills & Tools</h2>
   <ul class="list-none">
-    <li>
-      <label for="js-skill">JavaScript</label>
-      <progress id="js-skill" max="100" value="70" />
-    </li>
-    <li>
-      <label for="vue-skill">Vue / Vuex</label>
-      <progress id="vue-skill" max="100" value="80" />
-    </li>
-    <li>
-      <label for="react-skill">React</label>
-      <progress id="react-skill" max="100" value="66" />
-    </li>
-    <li>
-      <label for="nextjs-skill">Next.js</label>
-      <progress id="nextjs-skill" max="100" value="72" />
-    </li>
-    <li>
-      <label for="eleventy-skill">Eleventy</label>
-      <progress id="eleventy-skill" max="100" value="66" />
-    </li>
-    <li>
-      <label for="node-skill">Node / Express.js</label>
-      <progress id="node-skill" max="100" value="45" />
-    </li>
-    <li>
-      <label for="css-skill">SCSS</label>
-      <progress id="css-skill" max="100" value="75" />
-    </li>
-    <li>
-      <label for="design-skill">Inkscape / Figma</label>
-      <progress id="design-skill" max="100" value="62" />
-    </li>
-    <li>
+    {#each skills as skill}
+      <li>
+        <label for="css-skill">{skill.name}</label>
+        <progress id="css-skill" max="100" value={skill.proficiency} />
+      </li>
+    {/each}
+    <li class="pt-2">
       <div>Infrastructure / Hosting / DB</div>
       <p class="leading-8">
         {#each tools as tool}
